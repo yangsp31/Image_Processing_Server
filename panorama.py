718d33cfcc255446401904053960b36e5e02f8d5
yangsp_31 <yang_seongp31@naver.com>
2024-06-02 19:27:33 +0900

    오류처리

--- panorama.py
+++ panorama.py
@@ -12,9 +12,13 @@ def create_panorama(urls, cookie) :
             image = cv2.imdecode(image, cv2.IMREAD_COLOR)
             images.append(image)
 
-        stitcher = cv2.Stitcher.create()
-
-        result, panorama = stitcher.stitch(images)
+        if(len(images) > 1) :
+            stitcher = cv2.Stitcher.create()
+            result, panorama = stitcher.stitch(images)
+        else :
+            panorama = images[0]
+            result = True
+        
         panorama = cv2.resize(panorama, (2048, panorama.shape[0]))
         panorama = cv2.flip(panorama, 1)
         result, image = cv2.imencode('.jpg', panorama)
